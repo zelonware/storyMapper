fd37a6f760e910b70346e8561cb64631ee0f545d
Elena G <elena.guzbla@gmail.com>
2017-10-27 14:38:20 +0200

    intern., wip login + books

---
+++ app/src/main/java/com/geekstorming/storymapper/BooksActivity.java
@@ -0,0 +1,16 @@
+package com.geekstorming.storymapper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author Elena Guzman Blanco (Beelzenef) - 3d10Mundos
+ */
+public class BooksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_books);
+    }
+}
--- app/src/main/java/com/geekstorming/storymapper/LoginActivity.java
+++ app/src/main/java/com/geekstorming/storymapper/LoginActivity.java
@@ -1,13 +1,31 @@
 package com.geekstorming.storymapper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+/**
+ * @author Elena Guzman Blanco (Beelzenef) - 3d10Mundos
+ */
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+    }
+
+    public void onClick_EnterApp (View v) {
+
+        Intent unIntent = null;
+
+        switch (v.getId()) {
+            case R.id.btn_SignIn:
+                unIntent = new Intent(LoginActivity.this, BooksActivity.class);
+                break;
+        }
+
+        startActivity(unIntent);
     }
 }
