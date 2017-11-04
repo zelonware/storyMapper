aad48e1faf5fd2643a2c1d257063c2ea684b822e
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2017-11-04 14:27:30 +0100

    new character layout, adding fab -> characters layout

--- app/src/main/java/com/geekstorming/storymapper/CharactersActivity.java
+++ app/src/main/java/com/geekstorming/storymapper/CharactersActivity.java
@@ -1,7 +1,14 @@
 package com.geekstorming.storymapper;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import com.geekstorming.storymapper.adapters.CharacterAdapter;
 
 /**
  * Character Activity
@@ -9,9 +16,33 @@ import android.os.Bundle;
  */
 public class CharactersActivity extends AppCompatActivity {
 
+    private FloatingActionButton fab_addCharacter;
+    private Toolbar toolBar_Characters;
+    private ArrayAdapter<CharacterAdapter> characterAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_characters);
+
+        // Getting Views
+        fab_addCharacter = (FloatingActionButton) findViewById( R.id.fab_Characters );
+        toolBar_Characters = (Toolbar) findViewById(R.id.toolbar_Characters);
+
+        // Getting adapter
+
+        // Lets create new character!
+        fab_addCharacter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CharactersActivity.this, CharacterActivity.class));
+            }
+        });
+
+        // Setting toolbar
+        setSupportActionBar(toolBar_Characters);
+        // Setting adapter
+
+
     }
 }
