c95897848b00b6b0d7a447442ddbd60a3f9e3886
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2017-11-11 12:32:55 +0100

    wip settings + aboutme

---
+++ app/src/main/java/com/geekstorming/storymapper/AboutActivity.java
@@ -0,0 +1,12 @@
+package com.geekstorming.storymapper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
