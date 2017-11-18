2ecbda041ecbb5d43b4d4a4d79f66e0d138920b7
Elena G <elena.guzbla@gmail.com>
2017-11-18 14:47:40 +0100

    login, styles, see characters...

--- app/src/main/java/com/geekstorming/storymapper/ui/books/EditBookActivity.java
+++ app/src/main/java/com/geekstorming/storymapper/ui/books/EditBookActivity.java
@@ -1,5 +1,6 @@
 package com.geekstorming.storymapper.ui.books;
 
+import android.content.Intent;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 import com.geekstorming.storymapper.R;
 import com.geekstorming.storymapper.data.pojo.Book;
 import com.geekstorming.storymapper.data.repos.BookRepository;
+import com.geekstorming.storymapper.ui.characters.CharactersActivity;
 
 public class EditBookActivity extends AppCompatActivity {
 
@@ -32,24 +34,28 @@ public class EditBookActivity extends AppCompatActivity {
         loadEditableBook();
     }
 
-    private void initializeViews()
-    {
+    private void initializeViews() {
         tID_nWords = (TextInputEditText) findViewById(R.id.tID_nWords);
         tID_BookDesc = (TextInputEditText) findViewById(R.id.tID_BookDesc);
         tID_BookName = (TextInputEditText) findViewById(R.id.tID_BookName);
         spn_Genre = (Spinner) findViewById(R.id.spn_Genre);
     }
 
-    private void loadEditableBook()
-    {
+    private void loadEditableBook() {
         tID_BookName.setText(editableBook.getBookTitle());
         tID_BookDesc.setText(editableBook.getBookDesc());
     }
 
-    public void onClick_editBook(View v)
-    {
-        editableBook = new Book(3, tID_BookName.getText().toString(),
-                tID_BookDesc.getText().toString(), tID_BookDesc.getText().toString(), 0);
-        Toast.makeText(this, "Editando proyecto...", Toast.LENGTH_SHORT).show();
+    public void onClick_editBook(View v) {
+        switch (v.getId()) {
+            case R.id.btn_SeeCharacters:
+                startActivity(new Intent(EditBookActivity.this, CharactersActivity.class));
+                break;
+            case R.id.fab_BookEdited:
+                editableBook = new Book(3, tID_BookName.getText().toString(),
+                        tID_BookDesc.getText().toString(), tID_BookDesc.getText().toString(), 0);
+                Toast.makeText(this, "Editando proyecto...", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
