9ec1d7261a50ffb1482c9058585c739f57294842
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2018-01-05 12:15:48 +0100

    add toolbar, options menu missing

--- app/src/main/java/com/geekstorming/storymapper/ui/books/fragments/BookList_Fragment.java
+++ app/src/main/java/com/geekstorming/storymapper/ui/books/fragments/BookList_Fragment.java
@@ -1,12 +1,17 @@
 package com.geekstorming.storymapper.ui.books.fragments;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v4.app.ListFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +23,7 @@ import com.geekstorming.storymapper.adapters.BooksAdapter;
 import com.geekstorming.storymapper.base.BasePresenter;
 import com.geekstorming.storymapper.data.pojo.Book;
 import com.geekstorming.storymapper.ui.books.contracts.ListBookContract;
+import com.geekstorming.storymapper.ui.settings.SettingsActivity;
 import com.geekstorming.storymapper.utils.CommonDialog;
 
 import java.util.List;
@@ -34,6 +40,8 @@ public class BookList_Fragment extends ListFragment implements ListBookContract.
     private ListBookListener callback;
 
     BooksAdapter adapter;
+
+    Toolbar toolbar_books;
 
     public static BookList_Fragment newInstance(Bundle args) {
 
@@ -76,6 +84,8 @@ public class BookList_Fragment extends ListFragment implements ListBookContract.
         View rootView = inflater.inflate(R.layout.fragment_book_list, container, false);
 
         FloatingActionButton fab_AddNewBook = (FloatingActionButton) rootView.findViewById(R.id.fab_Books);
+        toolbar_books = (Toolbar) rootView.findViewById(R.id.toolbar_Books);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar_books);
 
         fab_AddNewBook.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -143,5 +153,24 @@ public class BookList_Fragment extends ListFragment implements ListBookContract.
         }
 
         return super.onContextItemSelected(item);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_activity_projects, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case R.id.action_settings:
+                startActivity(new Intent(getActivity(), SettingsActivity.class));
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
